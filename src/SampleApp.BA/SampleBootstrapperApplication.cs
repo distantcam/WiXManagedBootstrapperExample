9699a003dfd6a4357a7915d3f8ec2f86c50ae4fd
Cameron MacFarland <distantcam@gmail.com>
2012-03-02 20:18:03 +0800

    Implemented install/repair/uninstall in the managed bootstrapper.

--- src/SampleApp.BA/SampleBootstrapperApplication.cs
+++ src/SampleApp.BA/SampleBootstrapperApplication.cs
@@ -1,13 +1,10 @@
 ﻿using System.Windows;
-using Autofac;
 using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
 
 namespace SampleApp.BA
 {
     public class SampleBootstrapperApplication : BootstrapperApplication
     {
-        protected IContainer Container { get; private set; }
-
         protected override void Run()
         {
             Application app = new Application();
